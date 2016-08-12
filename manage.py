47a1a24ff984ec879ac274d1ae85c388b8a2cdb2
adyouri <raryat@gmail.com>
2016-08-12 16:09:47 +0100

    Add code coverage

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 from flask_script import Manager
 import os
+import unittest
+import coverage
 
 from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -11,6 +13,28 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+    """ Run unit tests """
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.command
+def cov():
+    """ Run Tests with coverage """
+    cov = coverage.coverage(branch=True, include="project/*")
+    cov.start()
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    cov.stop()
+    cov.save()
+    print 'Coverage Summary:'
+    cov.report()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    covedir = os.path.join(basedir, 'coverage')
+    cov.html_report(directory=covedir)
+    cov.erase()
 
 if __name__ == '__main__':
     manager.run()
